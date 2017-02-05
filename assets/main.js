0836c7c7cb3c4fd9301d5433b0be425f310b6eba
Amy Mok <amy.mok@cfpb.gov>
2017-02-05 02:06:50 -0500

    Add badge ajax call

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url:  'https://www.codeschool.com/users/amymok.json',
+    dataType: 'jsonp',
+    success: function(response) {
+        for (var i = 0; i < response.courses.completed.length; i++) {
+            var course = response.courses.completed[i]
+            textToAdd = "<div class=\"course\">" +
+                "<h3>" + course.title + "</h3>" +
+                "<img src=\"" + course.badge + "\">" +
+                "<a href=\"" + course.url + "\" target=_blank class=\"btn btn-primary\">See Course</a>" +
+                "</div>";
+            $("#badges").append(textToAdd);
+        }          
+    }
+  })
 
 });
